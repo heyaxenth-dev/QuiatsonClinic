6ba71ecd048e86e5f1d8923728247cfa93b187cb
heyaxenthDev <hyacynth.dev@gmail.com>
2025-09-05 17:19:31 +0800

    database update

--- database/quiatsonclinic.sql
+++ database/quiatsonclinic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 19, 2025 at 10:05 PM
+-- Generation Time: Sep 05, 2025 at 11:18 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -52,6 +52,7 @@ INSERT INTO `admin_staff` (`id`, `username`, `email`, `role`, `password`, `date_
 CREATE TABLE `appointments` (
   `id` int(11) NOT NULL,
   `patient_id` varchar(255) NOT NULL,
+  `patient_type` varchar(50) NOT NULL,
   `severity` varchar(150) NOT NULL,
   `lastname` varchar(50) DEFAULT NULL,
   `firstname` varchar(50) DEFAULT NULL,
@@ -68,7 +69,8 @@ CREATE TABLE `appointments` (
   `appointment_date` date DEFAULT NULL,
   `time_slot` varchar(50) DEFAULT NULL,
   `symptom` varchar(100) DEFAULT NULL,
-  `status` varchar(255) NOT NULL DEFAULT 'Pending',
+  `uploaded_id` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
   `remarks` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -77,13 +79,40 @@ CREATE TABLE `appointments` (
 -- Dumping data for table `appointments`
 --
 
-INSERT INTO `appointments` (`id`, `patient_id`, `severity`, `lastname`, `firstname`, `middle_initial`, `address`, `age`, `sex`, `birthdate`, `civil_status`, `phone`, `weight`, `height`, `bloodtype`, `appointment_date`, `time_slot`, `symptom`, `status`, `remarks`, `created_at`) VALUES
-(1, '05012025-01', 'Regular', 'asdas', 'sadasd', 's', 'sadsad', 24, 'Male', '2001-05-07', 'Single', '09898390281', '50', '150', 'A-', '2025-05-17', '10:30 AM - 11:30 AM', 'Fever', 'Approved', '', '2025-05-01 10:26:12'),
-(2, '05012025-02', 'Regular', 'asdas', 'sadasd', 's', 'sadsad', 24, 'Male', '2001-05-07', 'Single', '09898390281', '50', '150', 'A-', '2025-05-17', '10:30 AM - 11:30 AM', 'Fever', 'Pending', '', '2025-05-01 10:27:12'),
-(3, '05182025-03', 'Regular', 'akjdkj', 'lkjkldjaskl', 'k', 'kjsakdj', 15, 'Male', '2000-01-04', 'Single', '90909090090', '50', '150', 'A+', '2025-05-19', '8:30 AM - 9:30 AM', 'Chest Pain (Moderate to\\r\\n                                        severe)', 'Pending', '', '2025-05-18 03:54:39'),
-(4, '05182025-04', 'Urgent', 'sajhjdkjhsa', 'jhsjdh', 'k', 'jkasjdjsd', 15, 'Male', '2002-05-09', 'Single', '90909090090', '50', '162', 'B+', '2025-05-28', '9:30 AM - 10:30 AM', 'Abdominal Pain (Moderate to severe)', 'Pending', '', '2025-05-18 04:25:58'),
-(5, '05182025-05', 'Regular', 'ahsdhjah', 'hasdhasg', 'H', 'asmndhkjasd', 15, 'Male', '2002-05-09', 'Single', '90909090090', '50', '162', 'B+', '2025-05-28', '9:30 AM - 10:30 AM', 'Abdominal Pain (Moderate to severe)', 'Pending', '', '2025-05-18 04:27:32'),
-(6, '05182025-06', 'Urgent', 'ahsdhjah', 'hasdhasg', 'H', 'a,s.dma.', 15, 'Male', '2002-05-09', 'Single', '09090990', '50', '162', 'B+', '2025-05-28', '9:30 AM - 10:30 AM', 'Toxic Looking', 'Approved', '', '2025-05-20 04:27:58');
+INSERT INTO `appointments` (`id`, `patient_id`, `patient_type`, `severity`, `lastname`, `firstname`, `middle_initial`, `address`, `age`, `sex`, `birthdate`, `civil_status`, `phone`, `weight`, `height`, `bloodtype`, `appointment_date`, `time_slot`, `symptom`, `uploaded_id`, `status`, `remarks`, `created_at`) VALUES
+(1, '09042025-01', 'senior_pwd', 'Urgent', 'hasdhja', 'dhjasgdhjg', 'h', 'jdahjds4', 65, 'Female', '1955-12-25', 'Married', '09651168472', '50', '150', 'B+', '2025-09-05', '9:30 AM - 10:30 AM', 'Chest Pain', 'uploads/uploaded_ids/uploaded_id_68b949d77dce47.56443506.jpg', 'Concluded', 'Patient: dhjasgdhjg h. hasdhja\nAge: 65\nSex: Female\nAddress: jdahjds4\nCivil Status: Married\nPhone: 09651168472\nSchedule: Friday September 5, 2025 9:30 AM - 10:30 AM\nRemarks: Testing the remarks form', '2025-09-04 08:12:07'),
+(2, '09042025-02', 'regular', 'Regular', 'ahdjhasd', 'jsahdjh', 'j', 'ajhksdjas', 25, 'Female', '2000-08-04', 'Single', '09651168472', '50', '150', 'A+', '2025-09-12', '10:30 AM - 11:30 AM', 'Fever', '', 'Rescheduled', '', '2025-09-04 08:54:05'),
+(3, '09042025-03', 'regular', 'Regular', 'ahdjhasd', 'jsahdjh', 'j', 'shjdgahjsd', 25, 'Female', '2000-08-04', 'Single', '09651168472', '50', '150', 'A+', '2025-09-05', '10:30 AM - 11:30 AM', 'Fever', '', 'Approved', '', '2025-09-04 08:55:49'),
+(4, '09052025-04', 'senior_pwd', 'Regular', 'Dojillo', 'Hya Cynth', 'G', 'Villavert-Jimenez, Hamtic, Antique', 25, 'Female', '2000-08-04', 'Single', '09651168472', '65', '150', 'A+', '2025-09-08', '11:30 AM - 12:30 PM', 'Cough', '', 'Approved', '', '2025-09-05 08:26:05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
+
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `mobile_no` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `dob` varchar(255) NOT NULL,
+  `sex` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `password_token` varchar(255) NOT NULL,
+  `verification_status` int(11) NOT NULL DEFAULT 0,
+  `created_at` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `client`
+--
+
+INSERT INTO `client` (`id`, `firstname`, `lastname`, `mobile_no`, `email`, `dob`, `sex`, `password`, `address`, `password_token`, `verification_status`, `created_at`) VALUES
+(1, 'Hya Cynth', 'Dojillo', '09651168472', 'hyacynth.mulaveintern@gmail.com', '2000-08-04', 'Female', '$2y$10$xxE.Nn/y6jacC9IZ5vTn.O2f23BDlt/xXCXczJKNDEABJCMzSuwwO', 'Kksajdksajd', '', 0, ''),
+(2, 'asjdhjkas', 'hsdsakjdhk', '09651168472', 'hyacynth.dev@gmail.com', '2000-09-13', 'Female', '$2y$10$M0BdCeRmvWNKn8rOJb8vRephKRXNS83S4SsaMVUSXUNq5EMUnYcb6', 'jkashdjas\\r\\n', '', 0, '');
 
 --
 -- Indexes for dumped tables
@@ -104,6 +133,12 @@ ALTER TABLE `appointments`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -117,7 +152,13 @@ ALTER TABLE `admin_staff`
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
